96e4b801441b43159a95a294cc2bce0173b4bafb
rabeesyed462007 <rabeesyed46@gmail.com>
2022-08-01 22:54:17 +0530

    version 4.0

--- c.js
+++ c.js
@@ -9,13 +9,12 @@ var config = {
 flatpickr(".old", config);
 
 
-function toggle(b){b.value=(b.value=="ON")?"OFF":"ON";}
 
-var meta1 = document.getElementById("meta1");
-var meta2 = document.getElementById("meta2");
-var meta3 = document.getElementById("meta3");
-var meta4 = document.getElementById("meta4");
-var meta5 = document.getElementById("meta5");
-var meta6 = document.getElementById("meta6");
+// var meta1 = document.getElementById("meta1");
+// var meta2 = document.getElementById("meta2");
+// var meta3 = document.getElementById("meta3");
+// var meta4 = document.getElementById("meta4");
+// var meta5 = document.getElementById("meta5");
+// var meta6 = document.getElementById("meta6");
 
  
